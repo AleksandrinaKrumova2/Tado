0f1de5fe23632ed29a740391cdf136f1dc90192e
AleksandrinaKrumova2 <36706369+AleksandrinaKrumova2@users.noreply.github.com>
2018-04-02 17:50:05 +0300

    Add files via upload

---
+++ com/android/databinding/library/baseAdapters/C0503R.java
@@ -0,0 +1,11 @@
+package com.android.databinding.library.baseAdapters;
+
+public final class C0503R {
+
+    public static final class id {
+        public static final int dataBinding = 2131296543;
+        public static final int onAttachStateChangeListener = 2131296827;
+        public static final int onDateChanged = 2131296828;
+        public static final int textWatcher = 2131297059;
+    }
+}
