70b15cda4c69f137d5e3cf718afd0c8f788ac9a9
AleksandrinaKrumova2 <36706369+AleksandrinaKrumova2@users.noreply.github.com>
2018-04-03 17:36:59 +0300

    Delete TimePickerDialog$OnTimeSetListener.java

--- com/wdullaer/materialdatetimepicker/time/TimePickerDialog$OnTimeSetListener.java
+++
@@ -1,5 +0,0 @@
-package com.wdullaer.materialdatetimepicker.time;
-
-public interface TimePickerDialog$OnTimeSetListener {
-    void onTimeSet(TimePickerDialog timePickerDialog, int i, int i2, int i3);
-}
