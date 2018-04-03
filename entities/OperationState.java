817828fa12dc9f0b5d9e577a0824a96199d0323c
AleksandrinaKrumova2 <36706369+AleksandrinaKrumova2@users.noreply.github.com>
2018-04-03 15:39:45 +0300

    Delete OperationState.java

--- entities/OperationState.java
+++
@@ -1,8 +0,0 @@
-package com.tado.android.entities;
-
-public class OperationState {
-    public static final String RECORDING_STATE_FAILED = "FAILED";
-    public static final String RECORDING_STATE_FINISHED = "FINISHED";
-    public static final String RECORDING_STATE_IN_PROGRESS = "IN_PROGRESS";
-    public static final String RECORDING_STATE_NOT_STARTED = "NOT_STARTED";
-}
