9b9532a617408a63d58eddcb19fe2c595c8440cf
AleksandrinaKrumova2 <36706369+AleksandrinaKrumova2@users.noreply.github.com>
2018-04-03 15:34:41 +0300

    Delete CreateAcSettingCommandSetRecording.java

--- entities/CreateAcSettingCommandSetRecording.java
+++
@@ -1,9 +0,0 @@
-package com.tado.android.entities;
-
-public class CreateAcSettingCommandSetRecording {
-    String[] acModes;
-
-    public CreateAcSettingCommandSetRecording(String[] acModes) {
-        this.acModes = acModes;
-    }
-}
